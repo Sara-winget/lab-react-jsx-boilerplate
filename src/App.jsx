c9f663b2fcc7af33b5ee68b3a6f1ca902dc84487
unknown <sarawinget16@gmail.com>
2024-06-24 16:55:59 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -29,14 +29,21 @@ function App() {
   const images = imageData(); 
 
   return (
-    <div>
-      <h1>Kalvium Gallery</h1>
-      <img key={images[0].id} src={images[0].img} alt="" />
-      <img key={images[1].id} src={images[1].img} alt="" />
-      <img key={images[2].id} src={images[2].img} alt="" />
-      <img key={images[3].id} src={images[3].img} alt="" />
+    <div className='app'>
+      <h1 className='kalvium'>Kalvium Gallery</h1>
+      <div className='img'>
+
+      <div className='img1'>
+      <div><img  key={images[0].id} src={images[0].img} alt="" height={400}/></div>
+      <div><img key={images[1].id} src={images[1].img} alt="" height={400}/></div>
+      </div>
+      <div className='img2'>
+      <img key={images[2].id} src={images[2].img} alt="" height={400}/>
+      <img key={images[3].id} src={images[3].img} alt="" height={400} />
+      </div>
     </div>
-  );git
+      </div>
+  );
 }
 
 export default App;
